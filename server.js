46c4ee3b59f1f73d269ee3df93b30e10b35be4d3
sahl-lahouaoui <lahouaoui2008sahl@gmail.com>
2026-08-13 16:23:31 +0100

    Use hosting port

--- server.js
+++ server.js
@@ -28,6 +28,8 @@ app.get("/image/:name", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
